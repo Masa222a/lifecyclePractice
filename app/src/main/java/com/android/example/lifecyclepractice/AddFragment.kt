deae380af7940109feb20fa40a895cddfb929d41
Masa222a <guitarstring0716@gmail.com>
2022-08-11 11:45:12 +0900

    onResume確認

--- app/src/main/java/com/android/example/lifecyclepractice/AddFragment.kt
+++ app/src/main/java/com/android/example/lifecyclepractice/AddFragment.kt
@@ -6,76 +6,70 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.android.example.lifecyclepractice.databinding.FragmentMainBinding
+import com.android.example.lifecyclepractice.databinding.FragmentAddBinding
 
-class MainFragment : Fragment() {
-    private lateinit var binding: FragmentMainBinding
+class AddFragment : Fragment() {
+    lateinit var binding: FragmentAddBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMainBinding.inflate(inflater, container, false)
-        binding.viewPager.adapter = LifecyclePagerAdapter(this)
-        Log.d("onCreateView", "MainFragment")
-        binding.floatingActionButton.setOnClickListener {
-            parentFragmentManager.beginTransaction().apply {
-                replace(R.id.container, AddFragment())
-                addToBackStack(null)
-                commit()
-            }
+        binding = FragmentAddBinding.inflate(inflater, container, false)
+        binding.button2.setOnClickListener {
+            parentFragmentManager.popBackStack()
         }
         return binding.root
-
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("onCreate", "MainFragment")
+        Log.d("onCreate", "AddFragment")
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d("onViewCreated", "MainFragment")
+        Log.d("onViewCreated", "AddFragment")
     }
 
     override fun onViewStateRestored(savedInstanceState: Bundle?) {
         super.onViewStateRestored(savedInstanceState)
-        Log.d("onViewStateRestored", "MainFragment")
+        Log.d("onViewStateRestored", "AddFragment")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d("onStart", "MainFragment")
+        Log.d("onStart", "AddFragment")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("onResume", "MainFragment")
+        Log.d("onResume", "AddFragment")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d("onPause", "MainFragment")
+        Log.d("onPause", "AddFragment")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("onStop", "MainFragment")
+        Log.d("onStop", "AddFragment")
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        Log.d("onSaveInstanceState", "MainFragment")
+        Log.d("onSaveInstanceState", "AddFragment")
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        Log.d("onDestroyView", "MainFragment")
+        Log.d("onDestroyView", "AddFragment")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("onDestroy", "MainFragment")
+        Log.d("onDestroy", "AddFragment")
     }
+
 }
